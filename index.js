0070bcc3936793f73ba7f9f78172c94d2c7c5045
Johora99 <fatimatuzjohora1999@gmail.com>
2025-01-11 23:03:23 +0600

    all user data

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
+const jwt = require('jsonwebtoken');
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -14,7 +15,7 @@ app.get('/',async(req,res)=>{
 
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.3oeok.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -34,6 +35,14 @@ async function run() {
     const bistroBossCollection = client.db('bistroBoss').collection('menu');
     const reviewsCollection = client.db('bistroBoss').collection('review');
     const addToCartsCollection = client.db('bistroBoss').collection('cart');
+    const userCollection = client.db('bistroBoss').collection('user');
+    // jwt token generate ===============================
+    app.post('jwt',async(req,res)=>{
+      const user = req.body;
+      const token = jwt.sign(user,process.env.ACCESS_TOKEN_SECRET,{expiresIn:'1d'})
+      res.send({token})
+
+    })
     // all menu item =====================
     app.get('/menu',async(req,res)=>{
       const menu = await bistroBossCollection.find().toArray();
@@ -51,10 +60,54 @@ async function run() {
       const cartItems = await addToCartsCollection.find(query).toArray();
       res.send(cartItems);
     })
+    // get all user information ========================
+    app.get('/user',async(req,res)=>{
+      const result = await userCollection.find().toArray();
+      res.send(result);
+    })
+    // post user information ============================
+    app.post('/user',async(req,res)=>{
+      const newUser = req.body;
+      const email = newUser.user_email;
+      const query = {user_email : email};
+      const existentUser = await userCollection.findOne(query);
+      if(existentUser){
+        return res.send({message : 'user already exist', insertedId: null})
+      }else{
+        const result = await userCollection.insertOne(newUser);
+        res.send(result);
+      }
+    })
+    // update user ============================
+    app.patch('/user/:id',async(req,res)=>{
+      const id = req.params.id;
+      const query = {_id : new ObjectId(id)};
+      const update = {
+        $set : {
+          role : 'admin',
+        }
+      }
+      const result = await userCollection.updateOne(query,update);
+      res.send(result);
+    })
+    // delete user =============================
+    app.delete('/user/:id',async(req,res)=>{
+      const id = req.params.id;
+      const query = {_id : new ObjectId(id)};
+      const result = await userCollection.deleteOne(query);
+      res.send(result);
+    })
     // post add to cart item ===============================
     app.post('/cart',async(req,res)=>{
       const newItem = req.body;
       const result = await addToCartsCollection.insertOne(newItem);
+      res.send(result);
+    })
+    // delete cart item ============================
+    app.delete('/cart/:id',async(req,res)=>{
+      const id = req.params.id;
+      const query = {_id : new ObjectId(id)};
+      const result = await addToCartsCollection.deleteOne(query);
       res.send(result);
     })
   } finally {
